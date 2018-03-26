09c8712df9bdf4fdc0bda96e95fd7b98f6e81b8b
AdrienSa <adrien.sartori@alumni.univ-avignon.fr>
2018-03-26 11:12:25 +0200

    Modif fonction test

---
+++ src/test/java/fr/univavignon/rodeo/IAnimalTest.java
@@ -0,0 +1,19 @@
+package fr.univavignon.rodeo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * 
+ * @author uapv1501967
+ */
+@RunWith(MockitoJunitRunner.class)
+public final class IAnimalTest 
+{
+
+	@Test
+	public void testGetXp()
+	{
+		
+	}
+}
