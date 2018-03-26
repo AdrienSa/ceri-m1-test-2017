b5472ed3b36dd9217286d14bfd17ce5a611d6fcf
AdrienSa <adrien.sartori@alumni.univ-avignon.fr>
2018-03-26 11:00:55 +0200

    Ajout de mockito

---
+++ src/test/java/fr/univavignon/rodeo/IEnvironmentTest.java
@@ -0,0 +1,14 @@
+package fr.univavignon.rodeo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IEnvironmentTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
